3ce89144700d2489ffd609449e1375d9fb21a233
hajarkobi <hajarkobi280@email.com>
2026-03-30 10:22:50 +0100

    Restore CI/CD pipeline without Minikube

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-from flask import Flask, jsonify
-
-app = Flask(__name__)
-
-@app.route('/')
+# oufrom flask import Flask, jsonify
+vim app.py app = Flask(__name__)
+# ou
+code app.py  # si VS Code est installé@app.route('/')
 def home():
     return jsonify({
-        "message": "Hello DevOps!",
+        "message": "Hello from my DevOps App! 🚀",
         "status": "ok",
         "version": "1.0"
     })
